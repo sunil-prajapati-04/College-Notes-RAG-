aa12791e45804a95d294f7c1224a05f20bebd8ad
Sunil Prajapati <2023cssunil14308@poornima.edu.in>
2026-05-21 19:06:31 +0530

    solve conflict of cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,10 +4,19 @@ import bodyParser from 'body-parser';
 import notesRoutes from './routes/note.route.js';
 import authRoutes from './routes/auth.route.js';
 import connectDB from './middelware/db.js';
+import cors from 'cors';
+import helmet from 'helmet';
 config();
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(cors({
+    origin:process.env.CLIENT_URL,
+    credentials:true,
+}));
+
+app.use(helmet());
 
 app.use(bodyParser.json());
 
